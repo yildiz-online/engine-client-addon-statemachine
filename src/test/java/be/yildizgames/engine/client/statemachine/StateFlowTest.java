c2a2741e5658825e01717f68faa47d961906e0f1
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-09-26 20:11:32 +0200

    [WIP] Sonar fixes

--- src/test/java/be/yildizgames/engine/client/statemachine/StateFlowTest.java
+++ src/test/java/be/yildizgames/engine/client/statemachine/StateFlowTest.java
@@ -28,8 +28,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 public class StateFlowTest {
 
     @Nested
